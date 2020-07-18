acd47f6c0a2b1b0255891d0686215730dc0753c1
yayar <ya.yarochkin@yandex.ru>
2020-07-19 00:30:43 +0100

    ferst commit 2lesson

---
+++ theblog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+# def home(reguest):
+#     return render(reguest, 'home.html', {})
+class HomeView(ListView):
+  model = Post
+  template_name='home.html'
+
+class ArticleDetailView(DetailView):
+  model = Post;
+  template_name = "article_details.html"
